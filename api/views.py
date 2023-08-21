5455c42962c97c5ea2131f0b3da61e1cb8b8b3a5
Bhagya shah <bhagyashah5122002@gmail.com>
2023-08-21 17:12:06 +0530

    attendance stats

--- api/views.py
+++ api/views.py
@@ -34,3 +34,57 @@ class AttendanceStatusView(APIView):
         result = list(db.attendances.aggregate(pipeline))
         print(result)
         return Response(result)
+
+
+
+class AttendanceStats(APIView):
+    def post(self, request, *args, **kwargs):
+        client_id = ObjectId(request.data.get("client_id"))
+        phase_id = ObjectId(request.data.get("phase_id"))
+        specified_date = request.data.get("date")
+        client = MongoClient("mongodb://65.2.116.84:27017/") 
+        print("success")
+        print(client_id)
+        print(phase_id)
+        print(specified_date)
+        db = client["production"]  
+        print("done")
+        pipeline = [
+            {
+                '$match': {
+                    'client': client_id,
+                    'phase': phase_id,
+                }
+            },
+            { "$addFields": { "dateStr": { "$dateToString": { "format": "%Y-%m-%d", "date": "$date" } } } },
+            { "$match": { "dateStr": specified_date } },
+            {
+                '$lookup': {
+                    'from': 'attendancestats', 
+                    'localField': 'stats', 
+                    'foreignField': '_id', 
+                    'as': 'result'
+                }
+            },
+            {
+                '$project': {
+                    'result.stats': 1
+                }
+            }
+        ]
+
+        result = list(db.attendances.aggregate(pipeline))
+        print(result)
+        if result:
+            response = result[0]["result"]
+            return Response({"attendance": response})
+        else:
+            return Response({"message": "No attendance data found for the specified criteria."})
+
+
+
+
+
+
+
+
--- pythonbackend/urls.py
+++ pythonbackend/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from api.views import AttendanceStatusView
+from api.views import AttendanceStats
 
 urlpatterns = [
     path('api/attendance-status/', AttendanceStatusView.as_view(), name='attendance-status'),
+    path('api/attendance-stats/', AttendanceStats.as_view(), name='AttendanceStats'),
+
     # Other URL patterns
 ]
