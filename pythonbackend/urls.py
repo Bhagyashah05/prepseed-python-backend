ff84450a9fde397c9af5f47ad22d1803cf7e59f2
Bhagya shah <bhagyashah5122002@gmail.com>
2023-08-24 17:03:48 +0530

    conflict resolved

--- pythonbackend/urls.py
+++ pythonbackend/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from api.views import AttendanceStatusView
 from api.views import AttendanceStats
+from api.views import clientSelect
 
 urlpatterns = [
     path('api/attendance-status/', AttendanceStatusView.as_view(), name='attendance-status'),
     path('api/attendance-stats/', AttendanceStats.as_view(), name='AttendanceStats'),
+    path('attendance/', clientSelect, name='Attendance'),
+
 
     # Other URL patterns
 ]
