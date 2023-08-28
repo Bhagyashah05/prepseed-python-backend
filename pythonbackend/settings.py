bbdea6538be49de8a7a8b7677490f2223e5ec26d
Bhagya shah <bhagyashah5122002@gmail.com>
2023-08-28 18:53:45 +0530

    changes in middleware3

--- pythonbackend/settings.py
+++ pythonbackend/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'yourapp.middleware.XFrameOptionsMiddleware', 
 ]
 
 ROOT_URLCONF = 'pythonbackend.urls'
