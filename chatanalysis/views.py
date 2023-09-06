e50fb0ad8908395e44e30fcbebe23603a6724d9d
Bhagya shah <bhagyashah5122002@gmail.com>
2023-09-06 16:16:49 +0530

    chatanalysis api created

--- chatanalysis/views.py
+++ chatanalysis/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from bson import ObjectId
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import json
+from datetime import datetime,timedelta
+from django.http import HttpResponse
+from django.shortcuts import render
 
-# Create your views here.
+client = MongoClient("mongodb://65.2.116.84:27017/") 
+db = client["production"] 
+
+
+class chatAnalysis(APIView):
+    def post(self, request, *args, **kwargs):
+        startDate = request.data.get('startDate')
+        endDate=request.data.get('endDate')
+        print(startDate)
+        print(endDate)
+        pipeline=[]
+        if startDate is not None:
+            pipeline.append({ "$addFields": { "dateStr": { "$dateToString": { "format": "%Y-%m-%d", "date": "$updatedAt" } } } })
+            date_match = {
+                "$gte": startDate
+            }
+            
+            if endDate is not None:
+                date_match["$lte"] = endDate
+            print(date_match)  
+            pipeline.append({ "$match": { "dateStr": date_match } })
+
+        result = list(db.conversations.aggregate(pipeline))
+        print(result)
+        Activegroups={}
+        for res in result:
+            Activegroups[str(res["_id"])]=res["name"]
+        return Response(Activegroups)
+
--- pythonbackend/urls.py
+++ pythonbackend/urls.py
@@ -3,6 +3,7 @@ from api.views import AttendanceStatusView
 from api.views import AttendanceStats
 from api.views import clientSelect,phaseSelect
 from api.views import StudentAttendanceAnalysis,Studentselect
+from chatanalysis.views import chatAnalysis
 
 urlpatterns = [
     path('api/attendance-status/', AttendanceStatusView.as_view(), name='attendance-status'),
@@ -10,6 +11,8 @@ urlpatterns = [
     path('attendance/', clientSelect, name='Attendance'),
     path('studAtt/', phaseSelect, name='phaseAttendance'),
     path('get_students/', Studentselect.as_view(), name='StudentAttendance'),
-    path('studentAttendanceAnalysis/',StudentAttendanceAnalysis.as_view(),name='StudentAttendanceAnalysis')
+    path('studentAttendanceAnalysis/',StudentAttendanceAnalysis.as_view(),name='StudentAttendanceAnalysis'),
+    path('api/chatAnalysis/',chatAnalysis.as_view(),name='chatAnalysis')
+
     # Other URL patterns
 ]
